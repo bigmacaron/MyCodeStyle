71d61bb0a4deac4ef17ef9c80a9c20bf1f830905
bigmacaron <semin8990@naver.com>
2025-05-15 08:55:44 +0900

    release 1.0.2 25-05-15

--- app/src/main/kotlin/kr/kro/fatcats/mycodestyle/MainActivity.kt
+++ app/src/main/kotlin/kr/kro/fatcats/mycodestyle/MainActivity.kt
@@ -19,8 +19,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-//        adManager = AdManager(this)
-//        adManager.startObserve(lifecycle)
+        adManager = AdManager(this)
+        adManager.startObserve(lifecycle)
         setContent {
             App(viewModel)
         }
